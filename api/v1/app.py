f485376e29059baf0a2412d4e472ee69eb626afa
RealKingTino <tmowizzy@gmail.com>
2023-10-27 12:19:55 -0700

    Aliyu -

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,7 +18,7 @@ def teardown_app_context(exception):
 
 @app.errorhandler(404)
 def not_found(error):
-    return jsonify({"error": "Not found"}), 404
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
