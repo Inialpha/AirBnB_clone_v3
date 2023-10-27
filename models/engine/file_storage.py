578bd8cb3649ff43360faf774b9471e940b6fb11
Initech <inimfonebong001@gmail.com>
2023-10-27 02:08:18 -0700

    Add ge and count methods for both db and file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,18 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """method retrive an object with <id> if ut exists"""
+        objs = self.all(cls)
+
+        key = cls.__name__ + "." + id
+
+        return objs.get(key, None)
+
+    def count(self, cls=None):
+        """counts objects of <cls> type or all if class is none"""
+        count = 0
+        for obj in self.all(cls):
+            count += 1
+        return (count)
