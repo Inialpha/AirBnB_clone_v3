81c41ea49e10d1fec229e188a5e05ed9e85825d7
Initech <inimfonebong001@gmail.com>
2023-10-28 10:19:58 +0100

    import make_respond

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,12 +3,13 @@
 import os
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown_app_context(exception):
@@ -19,6 +20,7 @@ def teardown_app_context(exception):
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({"error": "Not found"}), 404)
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
